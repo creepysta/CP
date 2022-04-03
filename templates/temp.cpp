b80b5a9c3d93312ffb82de8dc4e1bdb021719af1
creepysta <sweetnanispregnantghost@yahoo.com>
2022-04-03 17:20:28 +0530

    template updated

--- templates/temp.cpp
+++ templates/temp.cpp
@@ -3,15 +3,21 @@
 
 */
 #include <bits/stdc++.h>
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
 using namespace std;
-const int MOD = int(1e9) + 7;
+template <typename T>
+using tree_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
 #define int int64_t
+const int INF = int(1e12) + 7;
+const int MOD = int(1e9) + 7;
 #ifdef LOCAL
-#define FIO ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define FIO ;
 #define debug(args...) { string _s = #args; replace(_s.begin(), _s.end(), ',', ' '); stringstream _ss(_s); istream_iterator<string> _it(_ss); cerr << boolalpha << "[ "; err(_it, args); }
 #else
+#define FIO ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
 #define debug(args...) ;
-#define FIO ;
 #endif
 #define tmpt template < class T
 #define ostop ostream& operator<<(ostream& o
@@ -19,15 +25,12 @@ const int MOD = int(1e9) + 7;
 void err(istream_iterator<string> it) {cerr<<"\b\b ]\n";}
 tmpt , class... Args> void err(istream_iterator<string> it, T a, Args... args) { cerr<<*it<<": "<<a<< ", ";err(++it,args...);}
 tmpt > ostop , const vector<T> &x) itfr
-tmpt > ostop , const array<T, int> &x) itfr
 tmpt > ostop , const set<T> &x) itfr
 tmpt > ostop , const multiset<T> &x) itfr
 tmpt , class V> ostop , const map<T,V> &x) itfr
 tmpt , class V> ostop , const pair<T,V> &p) {o<<"(";o<<p.first<<", "<<p.second<<")";return o;}
 
 void solve() {
-	array<int, 3> a{1, 2, 3};
-	debug(a);
 }
 
 int32_t main() {
